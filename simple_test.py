9201ea67ec4ef29d2385c8d51f7196ecaee60d1c
Etienne Russeil <etru7215@localhost.localdomain>
2025-02-07 16:21:31 +0100

    restructure folders

--- simple_test.py
+++ simple_test.py
@@ -1,6 +1,6 @@
 import multiview as mv
 
-MvSR = mv.MvSR("datasets/", max_length=10, pop_size=100, generations=100, n_params=3)
+MvSR = mv.MvSR("datasets/linear/", max_length=10, pop_size=100, generations=100, n_params=3)
 MvSR.run()
 result_table = MvSR.raw_results
 MvSR.plot_all_fits()
