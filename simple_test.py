0be62391cfc265dc33dcfee4f1ca02e9985a4275
Konstantin Malanchev <hombit@gmail.com>
2025-04-10 11:05:29 -0400

    Apply ppt

--- simple_test.py
+++
@@ -1,6 +0,0 @@
-import multiview as mv
-
-MvSR = mv.MvSR("datasets/linear/", max_length=10, pop_size=100, generations=100, n_params=3)
-MvSR.run()
-result_table = MvSR.raw_results
-MvSR.plot_all_fits()
